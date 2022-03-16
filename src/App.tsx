69c751acee62e60915c7e9ab6aee8d34436ef797
unknown <roven121@gmail.com>
2022-03-16 12:46:36 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {  useState } from 'react';
+import MainBar from './components/main/MainBar';
+import {GlobalState,AppState,Context} from "./Context/State"
 
 function App() {
-  return (
-    <div>
 
+  const [appState,setAppState] = useState<AppState>({
+    Auto:{
+      userId:"test",
+      jst:"fadfa"
+    }
+  })
+   return (
+    <div>
+    <GlobalState.Provider value= {{appState:appState,setAppState:setAppState as  any}}>
+      <MainBar/>
+    </GlobalState.Provider>
     </div>
   );
 }
--- src/components/main/MainBar.tsx
+++ src/components/main/MainBar.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-
+import React,{useContext} from 'react';
+import {GlobalState}from '../../Context/State'
 function MainBar() {
+  // To use Context Global you need To Improt GlobalState 
+  //The appState Save the Last State And setAppState Update the State
+const {appState,setAppState} = useContext(GlobalState)
+console.log(appState.Auto?.jst);
+
   return (
     <div>
+      {appState.Auto?.jst}
 
     </div>
   );
