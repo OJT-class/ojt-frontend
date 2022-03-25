3bce8ddd7647a19f44d2c7a0f4ea4515b3752c38
amabelle <amabelletr@gmail.com>
2022-03-26 01:32:48 +0300

    Starter avatar with dropdown menu

--- src/components/shared/navigation/MainNavigation/Navbar.tsx
+++ src/components/shared/navigation/MainNavigation/Navbar.tsx
@@ -1,18 +1,33 @@
 import React from 'react'
 
-import { Avatar, Image } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import "antd/dist/antd.css";
+import { Avatar, Image, Dropdown, Menu } from 'antd';
+import { UserOutlined, SolutionOutlined, LockOutlined, PoweroffOutlined } from '@ant-design/icons';
 
 import * as S from './styles';
 
 const Navbar: React.FC = () => {
+
+const widgetMenu = (
+  <Menu>
+    <Menu.Item>
+      <SolutionOutlined style={{textAlign: 'center', paddingRight: '0.5rem'}} />
+      profile
+    </Menu.Item>
+    <Menu.Item>
+      <PoweroffOutlined style={{textAlign: 'center', paddingRight: '0.5rem'}} />
+      sign out
+    </Menu.Item>
+  </Menu>
+);
+
   return (
      <S.Nav>
-    {/* <div> */}
-        <S.NavBarTitle>ToDos Board Customized Title</S.NavBarTitle>
-        <Avatar icon={<UserOutlined />} style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}/>
-        {/* <Avatar src={<Image src="https://joeschmoe.io/api/v1/random" style={{ width: 32 }} />} /> */}
-        {/* </div> */}
+      <S.NavBarTitle>ToDos Board Customized Title</S.NavBarTitle>
+      <Dropdown overlay={widgetMenu} placement="bottomRight" arrow={{ pointAtCenter: true }}>
+        <Avatar src={<Image src="https://joeschmoe.io/api/v1/random" style={{ width: 32 }} />} />
+        {/* <Avatar size={42} icon={<UserOutlined />} style={{ color: '#87d068' }}/> */}
+        </Dropdown>
      </S.Nav>
   )
 }
